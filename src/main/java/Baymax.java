03d89f11f85f93a93b3de4a0550ce83ea0302ccc
cyfake <imcyfake@gmail.com>
2025-08-20 21:15:56 +0800

    feat: add echo functionality from user input

--- src/main/java/Baymax.java
+++ src/main/java/Baymax.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Baymax {
     private static final String HORIZONTAL = "────────────────────────────────────────────────────────────";
 
@@ -6,11 +8,23 @@ public class Baymax {
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         printLine();
         System.out.println("""
                 Hello! I am Baymax, your personal chatbot companion.
                 I am here to help.""");
         printLine();
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            printLine();
+            System.out.println(input);
+            printLine();
+            input = scanner.nextLine();
+        }
+
         System.out.println("I will deactivate now.");
         printLine();
     }
