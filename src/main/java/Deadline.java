add4b6160ec5e7bc72ef6f324c7f2a6d5ce072e1
cyfake <imcyfake@gmail.com>
2025-08-21 18:44:04 +0800

    refactor: add Todo, Deadline and Event subclasses

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,8 @@
+public class Deadline extends Task {
+    protected String deadline;
+
+    public Deadline(String description, String deadline) {
+        super(description);
+        this.deadline = deadline;
+    }
+}
---
+++ src/main/java/Event.java
@@ -0,0 +1,10 @@
+public class Event extends Task {
+    protected String startTime;
+    protected String endTime;
+
+    public Event(String description, String startTime, String endTime) {
+        super(description);
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+}
