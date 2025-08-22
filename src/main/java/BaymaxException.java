c482bebc15ed00d82045f52adc7b416a841d9d6f
cyfake <imcyfake@gmail.com>
2025-08-22 10:59:12 +0800

    feat: add BaymaxException with nested classes

---
+++ src/main/java/BaymaxException.java
@@ -0,0 +1,41 @@
+public class BaymaxException extends Exception {
+    public BaymaxException(String msg) {
+        super(msg);
+    }
+
+    public static class InvalidCommandException extends BaymaxException {
+        public InvalidCommandException() {
+            super("Unknown command detected. Don’t worry, I am still here to help.");
+        }
+    }
+
+    public static class MissingDescriptionException extends BaymaxException {
+        public MissingDescriptionException(String command) {
+            super(String.format(
+                    "Pardon me. The \"%s\" command needs more details. " +
+                    "Please provide a description so I may assist you.", command));
+        }
+    }
+
+    public static class MissingDeadlineException extends BaymaxException {
+        public MissingDeadlineException() {
+            super("I am sorry, I could not find a deadline. " +
+                  "Please use: deadline <description> /by <deadline>.");
+        }
+    }
+
+    public static class MissingArgumentsException extends BaymaxException {
+        public MissingArgumentsException() {
+            super("I am unable to schedule this event without full details. " +
+                  "Please use: event <description> /from <start> /to <end>.");
+        }
+    }
+
+    public static class InvalidIndexException extends BaymaxException {
+        public InvalidIndexException(int index) {
+            super(String.format(
+                    "I have scanned your task list, but I cannot find a task numbered %d. " +
+                    "Please check again.", index + 1));
+        }
+    }
+}
