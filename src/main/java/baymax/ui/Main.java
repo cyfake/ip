7496a16976fa7a365ccc0d3d35285c881aa3b128
cyfake <imcyfake@gmail.com>
2025-09-14 12:44:56 +0800

    feat: Add app icon and title

--- src/main/java/baymax/ui/Main.java
+++ src/main/java/baymax/ui/Main.java
@@ -7,6 +7,7 @@ import baymax.MainWindow;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -23,6 +24,11 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+
+            stage.setTitle("Baymax");
+            Image icon = new Image(this.getClass().getResourceAsStream("/images/icon.png"));
+            stage.getIcons().add(icon);
+
             stage.setMinHeight(220);
             stage.setMinWidth(500);
             stage.setScene(scene);
